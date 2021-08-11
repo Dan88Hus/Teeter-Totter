83806d6de53f9959fe149559d61d42d626e54e8e
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2021-08-11 18:53:53 +0300

    auto indented

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,19 @@
 <template>
   <div class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container-fluid">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+    <div class="container-fluid">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Huseyin OZDOGAN</a>
+          <a class="nav-link active" aria-current="page" href="#"
+            >Huseyin OZDOGAN</a
+          >
         </li>
-        </ul>
-      </div>
-
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  
-};
+export default {};
 </script>
 
 <style lang="scss" scoped>
